58ceeae63d32506665de13cbf12fe82f40e94518
bcen707 <czarsalad707@gmail.com>
2014-11-25 09:58:42 -0800

    first commit

---
+++ js/menu.js
@@ -0,0 +1,43 @@
+var menuItems = [ 
+
+	{
+		item: 'Fried Chicken Wings (10 pcs.)',
+		price: 10.95,
+		category: 'Appetizer',
+		recommended: 'no'
+		spicy: 'no',
+
+	}
+	{
+		item: 'Egg Rolls (3 pcs.)',
+		price: 4.95,
+		category: 'Appetizer',
+		recommended: 'yes',
+		spicy: 'no'
+	}	
+	{
+		item:
+		price:
+		category:
+		recommended:
+		spicy: 
+	}
+	{
+		item:
+		price:
+		category:
+		recommended:
+		spicy: 
+	}
+	{
+		item:
+		price:
+		category:
+		recommended:
+		spicy: 
+	}
+
+
+
+
+]
