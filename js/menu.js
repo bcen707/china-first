57f8b421921aa547e2b5b7ddbd8f4d44fa7693bb
bcen707 <czarsalad707@gmail.com>
2014-11-30 19:07:37 -0800

    more items

--- js/menu.js
+++ js/menu.js
@@ -44,40 +44,128 @@ var menuItems = [
 		spicy: 'yes'
 	}
 	{	
+		item: 'General Tso\'s Chicken',
+		price: 10.95,
+		category: 'Chicken',
+		recommended: 'yes',
+		spicy: 'yes'
+	}
+	{	
+		item: 'Almond Fried Chicken',
+		price: 9.95,
+		category: 'Chicken',
+		recommended: 'no',
+		spicy: 'no'
+	}
+	{	
+		item: 'Mandarin Pork',
+		price: 10.95,
+		category: 'Pork',
+		recommended: 'no',
+		spicy: 'yes'
+	}
+	{	
+		item: 'Sweet and Sour Pork',
+		price: 9.95,
+		category: 'Pork',
+		recommended: 'no',
+		spicy: 'no'
+	}
+	{	
+		item: 'Orange Beef',
+		price: 11.95,
+		category: 'Beef',
+		recommended: 'no',
+		spicy: 'yes'
+	}
+	{	
+		item: 'Fresh Ginger Beef',
+		price: 10.95,
+		category: 'Beef',
+		recommended: 'no',
+		spicy: 'yes'
+	}
+	{	
+		item: 'Peppered Squid with Green Pepper',
+		price: 12.95,
+		category: 'Seafood',
+		recommended: 'no',
+		spicy: 'yes'
+	}
+	{	
+		item: 'Prawns with Lobster Sauce',
+		price: 10.95,
+		category: 'Seafood',
+		recommended: 'no',
+		spicy: 'no'
+	}
+	{	
 		item: '',
-		price:
+		price: ,
+		category: 'Veggie and Tofu',
+		recommended: '',
+		spicy: ''
+	}
+	{	
+		item: '',
+		price: ,
+		category: 'Veggie and Tofu',
+		recommended: '',
+		spicy: ''
+	}
+	{	
+		item: '',
+		price: ,
 		category: '',
 		recommended: '',
 		spicy: ''
 	}
 	{	
 		item: '',
-		price:
+		price: ,
 		category: '',
 		recommended: '',
 		spicy: ''
 	}
 	{	
 		item: '',
-		price:
+		price: ,
 		category: '',
 		recommended: '',
 		spicy: ''
 	}
 	{	
 		item: '',
-		price:
+		price: ,
 		category: '',
 		recommended: '',
 		spicy: ''
 	}
-	{	
-		tem: '',
-		price:
-		category: '',
-		recommended: '',
-		spicy: ''
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
