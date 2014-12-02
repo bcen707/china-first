e2ea3f8f365686f228aa521c9b8ebcc3b83c8019
Tanner Garrett <tanner@garrett.org>
2014-12-02 10:13:25 -0800

    Added basic map

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-"use strict";
+$(document).ready(function() {
+	"use strict";
+	var latitudeDefault = 47.659407;
+	var longitudeDefault = -122.313306;
+	var mapOptions = {
+		center: {lat: latitudeDefault, lng: longitudeDefault},
+		zoom: 4
+	};
+	var map = new google.maps.Map(document.getElementById('maparea'), mapOptions);
+
+
+});
 
 angular.module('Menu', [])
 	.controller('MenuController', function($scope) {
 	$scope.menu = menuItems;
 
 
-
-
-	});
+});	
