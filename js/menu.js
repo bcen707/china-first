268cff3c29d112bde8245941e47243e7433c688d
bcen707 <czarsalad707@gmail.com>
2014-12-03 13:52:33 -0800

    modified item array, need to implement sorting in html

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,8 @@ var menuItems = [
 		price: 10.95,
 		category: 'Appetizer',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile1.jpg'
 
 	},
 	{
@@ -15,146 +16,167 @@ var menuItems = [
 		price: 4.95,
 		category: 'Appetizer',
 		recommended: 'yes',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile2.jpg'
 	},	
 	{
 		item: 'Honey Walnut Prawns',
 		price: 13.95,
 		category: 'House Specials',
 		recommended:'yes',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile3.jpg'
 	},
 	{
 		item: 'Kung Pao Three Delight',
 		price: 12.95,
 		category: 'House Specials',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile4.jpg'
 	},
 	{
 		item: 'Shanghai Won Ton Soup',
 		price: 7.95,
 		category: 'Soup',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile5.jpg'
 	},
 	{
 		item: 'Hot and Sour Soup',
 		price: 7.95,
 		category: 'Soup',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile6.jpg'
 	},
 	{	
 		item: 'General Tso\'s Chicken',
 		price: 10.95,
 		category: 'Chicken',
 		recommended: 'yes',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile7.jpg'
 	},
 	{	
 		item: 'Almond Fried Chicken',
 		price: 9.95,
 		category: 'Chicken',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile8.jpg'
 	},
 	{	
 		item: 'Mandarin Pork',
 		price: 10.95,
 		category: 'Pork',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile9.jpg'
 	},
 	{	
 		item: 'Sweet and Sour Pork',
 		price: 9.95,
 		category: 'Pork',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile10.jpg'
 	},
 	{	
 		item: 'Orange Beef',
 		price: 11.95,
 		category: 'Beef',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile11.jpg'
 	},
 	{	
 		item: 'Fresh Ginger Beef',
 		price: 10.95,
 		category: 'Beef',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile12.jpg'
 	},
 	{	
 		item: 'Peppered Squid with Green Pepper',
 		price: 12.95,
 		category: 'Seafood',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile13.jpg'
 	},
 	{	
 		item: 'Prawns with Lobster Sauce',
 		price: 10.95,
 		category: 'Seafood',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile14.jpg'
 	},
 	{	
 		item: 'Assorted Chinese Vegetables',
 		price: 8.95,
 		category: 'Veggie and Tofu',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile15.jpg'
 	},
 	{	
 		item: 'Eggplant with Garlic Sauce',
 		price: 8.95,
 		category: 'Veggie and Tofu',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile16.jpg'
 	},
 	{	
 		item: 'China First Fried Rice',
 		price: 10.95,
 		category: 'Fried Rice',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile17.jpg'
 	},
 	{	
 		item: 'Imperial Fried Rice',
 		price: 9.95,
 		category: 'Fried Rice',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile18.jpg'
 	},
 	{	
 		item: 'Pan Fried Chow Ho Fun',
 		price: 9.95,
 		category: 'Chow Mein',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile19.jpg'
 	},
 	{	
 		item: 'Soy Bean Hot Sauce Pork Noodle',
 		price: 7.95,
 		category: 'Chow Mein',
 		recommended: 'no',
-		spicy: 'yes'
+		spicy: 'yes',
+		picUrl: 'img/tile20.jpg'
 	},
 	{	
 		item: 'Roast Pork Egg Foo Young',
 		price: 9.99,
 		category: 'Egg Foo Young',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile21.jpg'
 	},
 	{	
 		item: 'Shrimp Egg Foo Young',
 		price: 10.99,
 		category: 'Egg Foo Young',
 		recommended: 'no',
-		spicy: 'no'
+		spicy: 'no',
+		picUrl: 'img/tile22.jpg'
 	}
 ];
