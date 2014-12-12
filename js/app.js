0cd28f1d55f4f272f0f8e3b2c70297b0732afa89
bcen707 <czarsalad707@gmail.com>
2014-12-11 21:21:11 -0800

    got carousel for front page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('Menu', ['ReviewApp'])
+angular.module('Menu', ['ReviewApp', 'Carousel'])
 	.controller('MenuController', function($scope) {
 	$scope.menu = menuItems;
 
@@ -13,24 +13,26 @@ angular.module('Menu', ['ReviewApp'])
 var today = new Date();
 var storeHours = new Date();
 
+var timeNow = today.getTime()
 
-/*
+
+
 	
-angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('FoodCarousel', function ($scope) {
-  $scope.myInterval = 5000;
-  var slides = $scope.slides = [];
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: 'http://placekitten.com/' + newWidth + '/300',
-      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-  };
-  for (var i=0; i<4; i++) {
-    $scope.addSlide();
-  }
-});
+angular.module('Carousel', ['ui.bootstrap'])
+  .controller('FoodCarousel', function ($scope) {
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+        image: 'http://placekitten.com/' + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      });
+    };
+    for (var i=0; i<4; i++) {
+      $scope.addSlide();
+    }
+  });
  
- */
+ 
