2166cc78983043d64bb601b78e16ad06fdc7e5d5
bcen707 <czarsalad707@gmail.com>
2014-12-11 19:12:33 -0800

    made recommended and spicy boolean values

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,8 @@ var menuItems = [
 		name: 'Fried Chicken Wings (10 pcs.)',
 		price: '$10.95',
 		category: 'Appetizer',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile1.jpg'
 
 	},
@@ -15,168 +15,168 @@ var menuItems = [
 		name: 'Egg Rolls (3 pcs.)',
 		price: '$4.95',
 		category: 'Appetizer',
-		recommended: 'yes',
-		spicy: 'no',
+		recommended: true,
+		spicy: false,
 		picUrl: 'img/tile2.jpg'
 	},	
 	{
 		name: 'Honey Walnut Prawns',
 		price: '$13.95',
 		category: 'House Specials',
-		recommended:'yes',
-		spicy: 'no',
+		recommended: true,
+		spicy: false,
 		picUrl: 'img/tile3.jpg'
 	},
 	{
 		name: 'Kung Pao Three Delight',
 		price: '$12.95',
 		category: 'House Specials',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile4.jpg'
 	},
 	{
 		name: 'Shanghai Won Ton Soup',
 		price: '$7.95',
 		category: 'Soup',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile5.jpg'
 	},
 	{
 		name: 'Hot and Sour Soup',
 		price: '$7.95',
 		category: 'Soup',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile6.jpg'
 	},
 	{	
 		name: 'General Tso\'s Chicken',
 		price: '$10.95',
 		category: 'Chicken',
-		recommended: 'yes',
-		spicy: 'yes',
+		recommended: true,
+		spicy: true,
 		picUrl: 'img/tile7.jpg'
 	},
 	{	
 		name: 'Almond Fried Chicken',
 		price: '$9.95',
 		category: 'Chicken',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile8.jpg'
 	},
 	{	
 		name: 'Mandarin Pork',
 		price: '$10.95',
 		category: 'Pork',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile9.jpg'
 	},
 	{	
 		name: 'Sweet and Sour Pork',
 		price: '$9.95',
 		category: 'Pork',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile10.jpg'
 	},
 	{	
 		name: 'Orange Beef',
 		price: '$11.95',
 		category: 'Beef',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile11.jpg'
 	},
 	{	
 		name: 'Fresh Ginger Beef',
 		price: '$10.95',
 		category: 'Beef',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile12.jpg'
 	},
 	{	
 		name: 'Peppered Squid with Green Pepper',
 		price: '$12.95',
 		category: 'Seafood',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile13.jpg'
 	},
 	{	
 		name: 'Prawns with Lobster Sauce',
 		price: '$10.95',
 		category: 'Seafood',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile14.jpg'
 	},
 	{	
 		name: 'Assorted Chinese Vegetables',
 		price: '$8.95',
 		category: 'Veggie and Tofu',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile15.jpg'
 	},
 	{	
 		name: 'Eggplant with Garlic Sauce',
 		price: '$8.95',
 		category: 'Veggie and Tofu',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile16.jpg'
 	},
 	{	
 		name: 'China First Fried Rice',
 		price: '$10.95',
 		category: 'Fried Rice',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile17.jpg'
 	},
 	{	
 		name: 'Imperial Fried Rice',
 		price: '$9.95',
 		category: 'Fried Rice',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile18.jpg'
 	},
 	{	
 		name: 'Pan Fried Chow Ho Fun',
 		price: '$9.95',
 		category: 'Chow Mein',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile19.jpg'
 	},
 	{	
 		name: 'Soy Bean Hot Sauce Pork Noodle',
 		price: '$7.95',
 		category: 'Chow Mein',
-		recommended: 'no',
-		spicy: 'yes',
+		recommended: false,
+		spicy: true,
 		picUrl: 'img/tile20.jpg'
 	},
 	{	
 		name: 'Roast Pork Egg Foo Young',
 		price: '$9.99',
 		category: 'Egg Foo Young',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile21.jpg'
 	},
 	{	
 		name: 'Shrimp Egg Foo Young',
 		price: '$10.99',
 		category: 'Egg Foo Young',
-		recommended: 'no',
-		spicy: 'no',
+		recommended: false,
+		spicy: false,
 		picUrl: 'img/tile22.jpg'
 	}
 ];
